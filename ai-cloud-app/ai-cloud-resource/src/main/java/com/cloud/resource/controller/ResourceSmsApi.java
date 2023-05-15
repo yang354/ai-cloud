ee9c23579378c1889741e35daa5b8f8f17f4ec78
yzz <yzz@gmail.com>
2023-05-15 13:47:44 +0800

    初始化

---
+++ ai-cloud-app/ai-cloud-resource/src/main/java/com/cloud/resource/controller/ResourceSmsApi.java
@@ -0,0 +1,81 @@
+package com.cloud.resource.controller;
+
+import cn.hutool.core.util.RandomUtil;
+import com.cloud.common.constant.CacheConstants;
+import com.cloud.common.constant.Constants;
+import com.cloud.common.constant.LangConstants;
+import com.cloud.common.domain.R;
+import com.cloud.common.utils.SpringUtils;
+import com.cloud.log.service.AsyncLogService;
+import com.cloud.mail.service.MailTemplate;
+import com.cloud.mail.service.impl.MailTemplateImpl;
+import com.cloud.redis.service.RedisService;
+import com.cloud.redis.utils.RedisLock;
+import com.cloud.resource.param.PhoneParam;
+import com.cloud.sms.config.properties.SmsProperties;
+import com.cloud.sms.domain.SmsResult;
+import com.cloud.sms.service.SmsTemplate;
+import com.cloud.sms.service.impl.TencentSmsTemplate;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 短信服务
+ *
+ * @author ai-cloud
+ */
+@Api(tags = "短信服务")
+@RestController
+@RequestMapping("sms")
+public class ResourceSmsApi {
+
+    @Autowired
+    private RedisService redisService;
+
+    @Autowired
+    private SmsProperties smsProperties;
+
+    private Long validTime = Constants.CAPTCHA_EXPIRATION;
+
+
+    /**
+     * 短信验证码
+     *
+     * @param param    发送验证码参数
+     * @param language 语言类型
+     */
+    @PostMapping("/login/code")
+    @ApiOperation(value = "快捷登录验证码", notes = "快捷登录验证码")
+    public R<?> smsCaptcha(@RequestBody PhoneParam param,
+                           @RequestHeader(value = LangConstants.LANGUAGE, required = false)
+                           @ApiParam(value = "语言类型", example = "zh-CN") String language) {
+        if (!smsProperties.getEnabled()) {
+            return R.fail("当前系统没有开启短信功能");
+        }
+        System.out.println(SpringUtils.getBean(RedisLock.class).tryLock("aaa",10000l)+"~~~~~~~~~");
+        String code = RandomUtil.randomNumbers(6);
+        // 保存验证码信息
+        String key = CacheConstants.CAPTCHA_CODE_KEY + param.getPhone();
+        redisService.setCacheObject(key, code, validTime, TimeUnit.MINUTES);
+        // 验证码模板id 自行处理 (查数据库或写死均可)
+        String templateId = "1579380";
+        Map<String, String> map = new HashMap<>(1);
+        map.put("code", code);
+        SmsTemplate smsTemplate = SpringUtils.getBean(SmsTemplate.class);
+        SmsResult result = smsTemplate.send(param.getPhone(), templateId, map);
+        if (!result.getIsSuccess()) {
+            return R.fail(result.getMessage());
+        }
+        //Object result = smsTemplate.send(param.getPhone(), templateId, map);
+        return R.ok(result);
+    }
+
+
+}
